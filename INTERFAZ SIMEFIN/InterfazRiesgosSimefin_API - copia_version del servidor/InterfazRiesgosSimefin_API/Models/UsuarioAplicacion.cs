57d30ea9cb3750f40fc1107b92bee7a96ca85738
vianey.carrillo <vianey.carrillo@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-01-17 17:24:12 +0000

    se elimina version
    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@424 96431cdd-213c-f94b-b4e9-a1b9f3093d70

--- INTERFAZ SIMEFIN/InterfazRiesgosSimefin_API - copia_version del servidor/InterfazRiesgosSimefin_API/Models/UsuarioAplicacion.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace InterfazRiesgosSimefin_API.Models
-{
-    public class UsuarioAplicacion : IdentityUser
-    {
-        public string Nombres {  get; set; }   
-    }
-}
