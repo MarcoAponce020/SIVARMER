a304be927a30ac4e7e39ea474bc9f7d98b0647a7
onepro02 <onepro02@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-04-22 22:09:48 +0000

    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@734 96431cdd-213c-f94b-b4e9-a1b9f3093d70

---
+++ Maincore2024/RiesgosSimefin_Oracle/Riesgos.Simefin/Riesgos.Simefin.API/Controllers/PortafoliosController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Riesgos.Simefin.Application.DTOs;
+using Riesgos.Simefin.Application.DTOs.Portfolio;
+using Riesgos.Simefin.Application.Interfaces.Portfolio;
+using Riesgos.Simefin.Domain.Entities;
+using System.Net;
+
+namespace Riesgos.Simefin.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortafoliosController : ControllerBase
+    {
+
+        private readonly IPortfolioUseCase _portfolioUseCase;
+        //private readonly IMapper _mapper;
+        private ResponseDTO _response = new ResponseDTO();
+
+        public PortafoliosController(IPortfolioUseCase portfolioUseCase)
+        {
+            _portfolioUseCase = portfolioUseCase;
+            //_mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Obtener todos los portafolios
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Portafolio>>> GetAllPortfolios()
+        {
+            var portafolios = await _portfolioUseCase.GetAllPortfolio();
+            return Ok(portafolios);
+        }
+
+        /// <summary>
+        /// Obtener todos los portafolios de acuerdo a una fecha
+        /// </summary>
+        /// <param name="fechaPosicion">Fecha de posición</param>
+        /// <returns></returns>
+        [HttpGet("fechaPosicion:string", Name = "GetPortafolioFecha")]
+        public async Task<IActionResult> GetPortfolioByDate(string fechaPosicion)
+        {
+
+            if (fechaPosicion == null || fechaPosicion == "0")
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsExitoso = false;
+                _response.ErrorMessages.Add("Se debe ingresar una fecha válida");
+                return this.GetStatusResponse(_response);
+            }
+
+            if (fechaPosicion.Length < 8)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsExitoso = false;
+                _response.ErrorMessages.Add("El formato de la fecha no es válido, Se espera una fecha de la siguiente manera: yyyyMMdd ");
+                return this.GetStatusResponse(_response);
+            }
+
+            _response = await _portfolioUseCase.GetPortfolioByDate(fechaPosicion);
+
+            return this.GetStatusResponse(_response);
+        }
+
+        /// <summary>
+        /// Obtener portafolio por Identificador
+        /// </summary>
+        /// <param name="idPortafolio"></param>
+        /// <returns></returns>
+        [HttpGet("{idPortafolio}")]
+        public async Task<IActionResult> GetPortfolioById(int idPortafolio)
+        {
+            _response = await _portfolioUseCase.GetPortfolioById(idPortafolio);
+            return this.GetStatusResponse(_response);
+        }
+
+        ///// <summary>
+        ///// Crear nuevo Portafolio
+        ///// </summary>
+        ///// <param name="request">Datos del Portafolio</param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public async Task<ActionResult<int>> CreatePortfolio(PortafolioCreateDTO request)
+        //{
+        //    var id = await _portfolioUseCase.CreatePortfolio(request);
+        //    return CreatedAtAction(nameof(GetPortfolioById), new { id }, id);
+        //}
+
+        /// <summary>
+        /// Actualización de un portafolio
+        /// </summary>
+        /// <param name="idPortafolio">Identificador</param>
+        /// <param name="request">Contenedor con información de Portafolio</param>
+        /// <returns></returns>
+        [HttpPut("{idPortafolio}")]
+        public async Task<IActionResult> UpdatePortfolio(int idPortafolio, PortafolioUpdateDTO request)
+        {
+            if (idPortafolio == 0 || (request == null || string.IsNullOrEmpty(request.ListaDatos)))
+            {
+                _response.IsExitoso = false;
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                this.GetStatusResponse(_response);
+            }
+
+            _response = await _portfolioUseCase.UpdatePortfolio(idPortafolio, request!);
+
+            return this.GetStatusResponse(_response);
+        }
+
+        /// <summary>
+        /// Eliminar un Portafolio
+        /// </summary>
+        /// <param name="idPortafolio">Identificador de Portafolio</param>
+        /// <returns></returns>
+        [HttpDelete("{idPortafolio}")]
+        public async Task<IActionResult> DeletePortfolio(int idPortafolio)
+        {
+            _response = await _portfolioUseCase.DeletePortfolio(idPortafolio);
+            return this.GetStatusResponse(_response);
+        }
+
+        /// <summary>
+        /// Carga de archivo de excel con registros de portafolios
+        /// </summary>
+        /// <param name="file">Archivo de Excel</param>
+        /// <returns></returns>
+        [HttpPost("ExcelLoad")]
+        public async Task<IActionResult> ExcelLoad(IFormFile file)
+        {
+
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No se proporcionó un archivo válido.");
+            }
+
+            _response = await _portfolioUseCase.ExcelLoad(file);
+
+            return this.GetStatusResponse(_response);
+        }
+
+        /// <summary>
+        /// Obteniendo el Status final a retornar
+        /// </summary>
+        /// <param name="response">Respuesta del servicio</param>
+        /// <returns></returns>
+        private IActionResult GetStatusResponse(ResponseDTO response)
+        {
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.BadRequest:
+                    return BadRequest(response);
+                case HttpStatusCode.OK:
+                    return Ok(response);
+                case HttpStatusCode.NotFound:
+                    return NotFound(response);
+                default:
+                    return Ok();
+            }
+        }
+
+
+    }
+
+}
---
+++ Maincore2024/RiesgosSimefin_Oracle/Riesgos.Simefin/Riesgos.Simefin.Application/DTOs/User/TokenDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Riesgos.Simefin.Application.DTOs.User
+{
+
+    public class TokenDTO
+    {
+
+        [Required]
+        public string AccessToken { get; set; } = string.Empty;
+
+        [Required]
+        public string RefreshToken { get; set; } = string.Empty;
+
+    }
+
+}
