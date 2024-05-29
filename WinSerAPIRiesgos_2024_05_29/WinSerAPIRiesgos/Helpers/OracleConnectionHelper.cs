7380f65a1ef9310d61263879bd2d807e5e7360cc
onepro02 <onepro02@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-05-29 17:16:07 +0000

    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@776 96431cdd-213c-f94b-b4e9-a1b9f3093d70

---
+++ WinSerAPIRiesgos_2024_05_29/WinSerAPIRiesgos/Helpers/OracleConnectionHelper.cs
@@ -0,0 +1,22 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
+
+namespace WinSerAPIRiesgos.Helpers
+{
+
+    public class OracleConnectionHelper
+    {
+
+        public static OracleConnection GetConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["OracleConexion"].ConnectionString;
+            var connection = new OracleConnection(connectionString);
+
+            return connection;
+        }
+
+
+    }
+
+}
+
---
+++ WinSerAPIRiesgos_2024_05_29/WinSerAPIRiesgos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WinSerAPIRiesgos
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+
+            // If you don’t want the exe to run as console when double-clicked, then use:
+            //   if (System.Diagnostics.Debugger.IsAttached)
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine("Service running, press  to stop.");
+                ((Service1)ServicesToRun[0]).Start(null);
+                Console.ReadLine();
+                ((Service1)ServicesToRun[0]).Stop();
+            }
+            else
+            {
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
