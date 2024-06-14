f89a119c585c4d999d878db066b716e1ca3a2a47
onepro02 <onepro02@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-06-14 14:29:58 +0000

    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@787 96431cdd-213c-f94b-b4e9-a1b9f3093d70

---
+++ API_DERIVADOS_ETAPA4_2024_06_14/apiRiesgos/ENTITY/PosicionCalculoVAR.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class PosicionCalculoVAR
+    {
+        public string Fecha { get; set; }
+        public string Intencion { get; set; }
+        public int TipoOperacion { get; set; }
+        public string TipoValor { get; set; }
+        public string Emision { get; set; }
+        public string Serie { get; set; }
+        public int FechaVto { get; set; }
+        public decimal TasaCupon { get; set; }
+        public int D_vto { get; set; }
+        public int DxV { get; set; }
+        public int TCupon { get; set; }
+       //Cambia nombre de Premio a TasaPremio
+        public decimal Premio { get; set; }
+        public long Titulos { get; set; }
+        public int TipoPosicion { get; set; }
+
+        //Se cambia tipo de dato public int PrecioCompra { get; set; }
+       // public int PrecioCompra { get; set; }
+        
+        public decimal PrecioCompra { get; set; }
+        public int FechaCompra { get; set; }
+        public string Mercado { get; set; }
+        public int NumPortafolio { get; set; }
+        public string Portafolio { get; set; }
+        //public int DxV { get; set; }
+        
+    }
+}
---
+++ API_DERIVADOS_ETAPA4_2024_06_14/apiRiesgos/ENTITY/Respuesta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITY
+{
+    public class Respuesta
+    {
+        public bool exito { get; set; }
+
+        public string mensaje { get; set; }
+
+        public List<string> ListaMensajes { get; set; } = new List<string>();
+
+    }
+}
