21d5b55fb245d1a3104360c8ed4c0e318cdbaea5
onepro02 <onepro02@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-06-26 23:48:42 +0000

    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@796 96431cdd-213c-f94b-b4e9-a1b9f3093d70

---
+++ RiesgosSimefin_Oracle_2024_06_26/Riesgos.Simefin/Riesgos.Simefin.Application/DTOs/Portfolio/PortafolioUpdateDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Riesgos.Simefin.Application.DTOs.Portfolio
+{
+
+    public class PortafolioUpdateDTO
+    {
+
+        public string? ListaDatos { get; set; }
+
+    }
+
+}
---
+++ RiesgosSimefin_Oracle_2024_06_26/Riesgos.Simefin/Riesgos.Simefin.Application/DTOs/User/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Riesgos.Simefin.Application.DTOs.User
+{
+
+    public class UserDTO
+    {
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Nombre { get; set; } = string.Empty;
+
+    }
+
+}
