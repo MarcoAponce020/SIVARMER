92c1edada6149418ad1b1d2bebd5ede0c023cd9c
onepro02 <onepro02@96431cdd-213c-f94b-b4e9-a1b9f3093d70>
2024-07-02 17:09:33 +0000

    git-svn-id: https://172.27.201.63/svn/TransicionNvaFSW/SST3/SIVARMER/Trunk/C%C3%B3digo@802 96431cdd-213c-f94b-b4e9-a1b9f3093d70

---
+++ RiesgosSimefin_Oracle_2024_07_01/Riesgos.Simefin/Riesgos.Simefin.Application/Mapping/AutoMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using Riesgos.Simefin.Application.DTOs.Portfolio;
+using Riesgos.Simefin.Application.DTOs.User;
+using Riesgos.Simefin.Domain.Entities;
+
+namespace Riesgos.Simefin.Application.Mapping
+{
+
+    public class AutoMappingProfile : Profile
+    {
+
+        public AutoMappingProfile()
+        {
+            CreateMap<Portafolio, PortafolioDTO>().ForMember(x => x.ListaDatos, list => list.MapFrom(ListaDatosMAP));
+            CreateMap<PortafolioDTO, Portafolio>();
+
+            CreateMap<Portafolio, PortafolioCreateDTO>().ReverseMap();
+            CreateMap<Portafolio, PortafolioUpdateDTO>().ReverseMap();
+            CreateMap<Usuario, UserDTO>().ReverseMap();
+        }
+
+        public List<ListaDatosDTO> ListaDatosMAP(Portafolio portafolio, PortafolioDTO portafolioDto)
+        {
+            var source = JsonConvert.DeserializeObject<List<ListaDatosDTO>>(portafolio.ListaDatos!);
+
+            return source!;
+        }
+
+    }
+
+}
---
+++ RiesgosSimefin_Oracle_2024_07_01/Riesgos.Simefin/Riesgos.Simefin.Infrastructure.Oracle/Repositories/UserRepository.cs
@@ -0,0 +1,96 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using Riesgos.Simefin.Application.Interfaces.User;
+using Riesgos.Simefin.Domain.Entities;
+
+namespace Riesgos.Simefin.Infrastructure.Oracle.Repositories
+{
+
+    /// <summary>
+    /// Clase que implementa los métodos de acceso a datos para Usuarios
+    /// </summary>
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly string? _connectionString = string.Empty;
+        private readonly string? _usersTableName = string.Empty;
+
+        public UserRepository(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetSection("ConnectionStrings").GetSection("OracleConexion").Value;
+            _usersTableName = _configuration.GetSection("DataBaseTables").GetSection("T_Usuarios").Value!;
+        }
+
+        /// <summary>
+        /// Agregar nuevo usuario
+        /// </summary>
+        /// <param name="entity">Contenedor con información del usuario</param>
+        /// <returns></returns>
+        public async Task<int> AddAsync(Usuario entity)
+        {
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var parameters = new
+                {
+                    //p_Id = entity.Id,
+                    p_UserName = entity.UserName,
+                    p_Nombre = entity.Nombre,
+                    p_Password = entity.Password,
+                    p_Rol = entity.Rol
+                };
+                var query = @$"INSERT INTO {_usersTableName} (UserName, Nombre, Password, Rol) 
+                            VALUES (:p_UserName, :p_Nombre, :p_Password, :p_Rol)";
+                var rowsAffected = await connection.ExecuteAsync(query, parameters);
+                return rowsAffected;
+            }
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Obtener información de todos los usuarios
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IEnumerable<Usuario>> GetAllAsync()
+        {
+            using (var connection = new OracleConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                string strQuery = @$"SELECT Id, UserName, Nombre, Password, Rol FROM {_usersTableName}";
+                return await connection.QueryAsync<Usuario>(strQuery);
+            }
+        }
+
+        /// <summary>
+        /// Obtener información de un usuario
+        /// </summary>
+        /// <param name="id">Identificador</param>
+        /// <returns></returns>
+        public async Task<Usuario> GetByIdAsync(int userId)
+        {
+            using (OracleConnection connection = new OracleConnection(_connectionString))
+            {
+                await connection.OpenAsync();
+                var parameters = new { p_Id = userId };
+                string strQuery = @$"SELECT Id, UserName, Nombre, Password, Rol FROM {_usersTableName} WHERE Id = :p_Id";
+                var user = await connection.QueryFirstOrDefaultAsync<Usuario>(strQuery, parameters);
+
+                return user!;
+            }
+        }
+
+        public Task<bool> UpdateAsync(Usuario entity)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+
+}
